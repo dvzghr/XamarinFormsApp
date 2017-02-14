42b1a1816834b82afdf164d180c3562922b8385b
thshr <ths_hr@hotmail.com>
2017-02-14 20:54:30 +0100

    XF update

--- XamarinFormsApp/XamarinFormsApp.Droid/Resources/Resource.Designer.cs
+++ XamarinFormsApp/XamarinFormsApp.Droid/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace XamarinFormsApp.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::XamarinFormsApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
